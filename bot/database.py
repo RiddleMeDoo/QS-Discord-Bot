531334f79b184aefc68da70d41ab8a89e9d79277
RiddleMeDoo <13726465+RiddleMeDoo@users.noreply.github.com>
2022-07-01 21:11:56 -0400

    Switch to built-in heroku redis db

--- bot/database.py
+++ bot/database.py
@@ -4,20 +4,21 @@ import os
 import json
 
 # Connect to database
-redis = redis.Redis(
-  host=os.getenv("REDIS_HOSTNAME"),
-  port=os.getenv("REDIS_PORT"),
-  password=os.getenv("REDIS_PASSWORD"),
-  decode_responses=True
-)
+# redis = redis.Redis(
+#   host=os.getenv("REDIS_HOSTNAME"),
+#   port=os.getenv("REDIS_PORT"),
+#   password=os.getenv("REDIS_PASSWORD"),
+#   decode_responses=True
+# )
+redisDB = redis.from_url(os.environ.get("REDIS_URL"))
 
 # Store
 def db_set(key, value):
-  redis.set(key, json.dumps(value))
+  redisDB.set(key, json.dumps(value))
 
 # Retrieval
 def db_get(key, default=None):
-  result = redis.get(key)
+  result = redisDB.get(key)
   if result is None:
     return default
   else:
@@ -29,14 +30,14 @@ def db_get(key, default=None):
 
 # Get all key/value pairs
 def db_get_all():
-  keys = redis.keys()
-  values = redis.mget(keys)
+  keys = redisDB.keys()
+  values = redisDB.mget(keys)
   return {keys[i]:json.loads(values[i]) for i in range(len(keys))}
 
 def db_set_all(dict):
   jsonDict = {key:json.dumps(dict[key]) for key in dict.keys()}
-  redis.mset(jsonDict)
+  redisDB.mset(jsonDict)
 
 # Deletion
 def db_del(key):
-  redis.delete(key)
+  redisDB.delete(key)
